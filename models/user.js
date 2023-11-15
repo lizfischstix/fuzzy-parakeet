903bfe5bd9d7c88324e4d1f90c199b647eb8b1d3
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-11-14 20:09:03 -0600

    working on models

--- models/user.js
+++ models/user.js
@@ -2,4 +2,39 @@ const { Schema, model } = require('mongoose');
 const thoughtSchema = require('./thought');
 const friendSchema = require('./friend');
 
-const userSchema = new Schema()
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+          },
+          id: false,
+    },
+);
+//HELP? THIS DOESN'T SEEM QUITE RIGHT.
+userSchema.virtual(`friendCount`).get(function(){
+    return count.User;
+})
+
+const User = model('user', userSchema);
+
+model.exports = User;
